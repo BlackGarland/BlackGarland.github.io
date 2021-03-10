ee129bc35e0b0f4c1d12dc01e7434d6bc1fc535a
BlackGarland <1626479137@qq.com>
2021-03-10 15:48:19 +0800

    Site updated: 2021-03-10 15:48:12

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const getRealPath = (pathname, desc = false) => {
+  if (!pathname) {
+    pathname = window.location.pathname;
+  }
+  let names = pathname.split("/");
+  if (desc === false) {
+    for (let i = names.length - 1; i >= 0; --i) {
+      let name = names[i].trim();
+      if (name.length > 0 && name !== "/" && name !== "index.html") {
+        return name;
+      }
+    }
+  } else {
+    for (let i = 0; i < names.length; ++i) {
+      let name = names[i].trim();
+      if (name.length > 0 && name !== "/" && name !== "index.html") {
+        return name;
+      }
+    }
+  }
+  return "/";
+};
+let links = document.querySelectorAll(".nexmoe-list-item");
+let rootRealPath = getRealPath(window.location.pathname, true);
+for (let link of links) {
+  let linkPath = link.getAttribute("href");
+  if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
+    link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
+  }
+}
+
+// 表格相册
+$("table")
+  .has("img")
+  .addClass("nexmoe-album");
+
+// 搜索
+function search() {
+  window.open($("#search_form").attr("action_e") + " " + $("#search_value").val());
+  return false;
+}
+
+// 平滑跳转同时修复锚点链接被转义
+$(document).ready(function () {
+
+  $("a.toc-link").click(function (ev) {
+    ev.preventDefault();
+    $("html, body").animate({
+      scrollTop: $(decodeURI($(this).attr("href"))).offset().top - 25
+    }, {
+      duration: 500,
+      easing: "swing"
+    });
+  });
+  return false;
+});
